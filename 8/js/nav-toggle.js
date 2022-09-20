4b5c543f5f6fdaf09cdf5c97cc7ffb6c9f1be25c
firefoxic <firefoxic.dev@gmail.com>
2022-09-20 18:07:16 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/610981-mishka-26@320848dbf097ac760ee623a80d1712521079d738 🚀

--- 8/js/nav-toggle.js
+++
@@ -1,1 +0,0 @@
-let nav=document.querySelector(".navigation"),navToggle=nav.querySelector(".navigation__toggle");nav.classList.remove("navigation--js-none"),nav.classList.add("navigation--is-closed"),navToggle.addEventListener("click",(()=>{nav.classList.toggle("navigation--is-closed")}));
